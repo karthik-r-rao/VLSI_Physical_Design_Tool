707f3aadb491f6689db79aeb2a918a3830a7d619
Karthik Rao <karthikrrao30@gmail.com>
2020-11-20 17:18:47 +0530

    added construct_graph

---
+++ src/partitioning/construct_graph.py
@@ -0,0 +1,28 @@
+"""
+@author: karthikrao
+
+Construct a networkx graph given its vertices and edges.
+
+"""
+
+import networkx as nx
+
+def construct_graph(nodes, edges):
+    """
+        construct_graph(nodes, edges)
+        
+        Parameters
+        ----------
+        nodes : List of nodes. 
+        edges : List of edges.
+        
+        Returns
+        ----------
+        
+        g : Networkx undirected graph. 
+    """
+
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+    return g
