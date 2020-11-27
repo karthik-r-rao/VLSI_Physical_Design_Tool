cd4f924f256a4111292d20a13af4f8b549a8cc56
Karthik Rao <karthikrrao30@gmail.com>
2020-11-27 15:09:17 +0530

    Create parse.py

---
+++ parse/parse.py
@@ -0,0 +1,14 @@
+"""
+@author: karthikrao
+
+Program to get graph from netlist.
+
+"""
+
+import networkx as nx
+import matplotlib.pyplot as plt
+from get_graph_info import *
+
+INFILE = "adder.isc" # specify path to .isc file 
+
+get_graph_info(INFILE)
